9df3e7db47f911b6dd88eab735e3f1394ea3ed64
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-12 09:46:01 +0200

    Update Windows build for v1.2.0

--- build_exe.bat
+++ build_exe.bat
@@ -1,24 +1,18 @@
 @echo off
-title CrossAim Power - Build EXE
-echo Installing build dependencies...
+title CrossAim Power v1.2.0 - Build EXE
 py -m pip install -U pyinstaller
 py -m pip install -r requirements.txt
 if errorlevel 1 pause & exit /b 1
 
-echo.
-echo Preparing CrossAim Power icon...
 py make_icon.py
 if errorlevel 1 pause & exit /b 1
 
-echo.
-echo Building CrossAim Power...
 pyinstaller --noconfirm --clean --windowed --onefile ^
   --name "CrossAim_Power" ^
   --icon "crossaim_power.ico" ^
   --add-data "crossaim_power.png;." ^
   --collect-all mss ^
-  crossaim_power.py
+  crossaim_power_v12.py
 
-echo.
 echo Finished. Check dist\CrossAim_Power.exe
 pause
