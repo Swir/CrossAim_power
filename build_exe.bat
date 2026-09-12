559056f398553597e109e82bcc9af6a4c07ef14b
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-12 09:10:14 +0200

    Fix Windows icon conversion and EXE build

--- build_exe.bat
+++ build_exe.bat
@@ -1,7 +1,12 @@
 @echo off
 title CrossAim Power - Build EXE
 echo Installing build dependencies...
-py -m pip install -U pyinstaller PySide6 mss
+py -m pip install -U pyinstaller PySide6 mss pillow
+if errorlevel 1 pause & exit /b 1
+
+echo.
+echo Preparing Windows icon...
+py -c "from PIL import Image; Image.open('crossaim_power.png').save('crossaim_power.ico', sizes=[(16,16),(32,32),(48,48),(64,64),(128,128),(256,256)])"
 if errorlevel 1 pause & exit /b 1
 
 echo.
@@ -9,7 +14,7 @@ echo Building CrossAim Power...
 pyinstaller --noconfirm --clean --windowed --onefile ^
   --name "CrossAim_Power" ^
   --icon "crossaim_power.ico" ^
-  --add-data "crossaim_power.ico;." ^
+  --add-data "crossaim_power.png;." ^
   --collect-all mss ^
   crossaim_power.py
 
