7bdaa7929524a9f9b01b3ea085b5f6027c369dd4
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-12 09:14:32 +0200

    Use deterministic icon generator for Windows build

--- build_exe.bat
+++ build_exe.bat
@@ -1,12 +1,13 @@
 @echo off
 title CrossAim Power - Build EXE
 echo Installing build dependencies...
-py -m pip install -U pyinstaller PySide6 mss pillow
+py -m pip install -U pyinstaller
+py -m pip install -r requirements.txt
 if errorlevel 1 pause & exit /b 1
 
 echo.
-echo Preparing Windows icon...
-py -c "from PIL import Image; Image.open('crossaim_power.png').save('crossaim_power.ico', sizes=[(16,16),(32,32),(48,48),(64,64),(128,128),(256,256)])"
+echo Preparing CrossAim Power icon...
+py make_icon.py
 if errorlevel 1 pause & exit /b 1
 
 echo.
